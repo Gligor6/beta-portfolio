a26906f9689c289a600ace8a4cec228320077363
Gligor6 <126977932+Gligor6@users.noreply.github.com>
2023-04-07 21:26:24 +0200

    First commit
    This is my first commit of my webstite version. It is not finished because it can't run on mobile or tablet devices and some buttons don't yet work.

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+window.onload = function consoleLog(){
+    console.log("Console working!")
+}
+
+const openFacebook = () => {
+    window.open("https://www.facebook.com/luka.gligorijevic.7/")
+}
+const openInstagram = () => { 
+    window.open("https://www.instagram.com/l_gligorijevic/")
+}
+
+function sendMail(){
+    window.open("mailto:gligorijevicluka8@gmail.com")
+}
